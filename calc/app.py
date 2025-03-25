81e8a690f4e70b43117a4ee527df3172051c7de4
aracope <156398622+aracope@users.noreply.github.com>
2025-03-25 16:53:57 -0700

    routes for Calc.

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,39 @@
-# Put your app in here.
+from flask import Flask, request
+from calc.operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "This is the calc homepage!"
+
+@app.route('/add')
+def addition():
+    a = float(request.args.get('a', 0))
+    b = float(request.args.get('b', 0))
+    result = add(a, b)
+    return str(result)
+
+@app.route('/sub')
+def subtraction():
+    a = float(request.args.get('a', 0))
+    b = float(request.args.get('b', 0))
+    result = sub(a, b)
+    return str(result)
+
+@app.route('/mult')
+def multiplication():
+    a = float(request.args.get('a', 0))
+    b = float(request.args.get('b', 0))
+    result = mult(a, b)
+    return str(result)
+
+@app.route('/div')
+def division():
+    a = float(request.args.get('a', 0))
+    b = float(request.args.get('b', 0))
+    result = div(a, b)
+    return str(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
